0a02134df65846ea284dc726dfd25fe91a9af0e4
liammukasa <wmukasa@gmail.com>
2021-03-26 13:35:23 +0300

    New updates

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'blog/home.html')
+def about(request):
+	return render(request,'blog/about.html',{'title':'about'})
