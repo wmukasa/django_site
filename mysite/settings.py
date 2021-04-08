6e1284c761b53a4ca5020f470e2a5aed293212ec
liammukasa <wmukasa@gmail.com>
2021-04-08 15:29:10 +0300

    Deleted static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '8vf#2@xc5t2^9y!hi%v+46i^p#-4r6rl-vq(c5oorswdb_c1q8'
 DEBUG = True
 
 #ALLOWED_HOSTS = ['myhotelsite.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myhotelsite.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -64,12 +64,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+'''
+DATABASES = {
+    'default': dj_database_url.config()
+       
+    
 }
 
 DATABASES = {
@@ -79,15 +85,10 @@ DATABASES = {
         'USER':'evscomuphxjuzy  ',
         'PASSWORD':'ad1edf9c15a1c284298f935e37aba10fe23adf018b8469071ee2fef3bf72137e',
         'HOST':'ec2-54-235-108-217.compute-1.amazonaws.com',
-        'PORT':5432
+        'PORT':'5432'
     }
 }
 '''
-DATABASES = {
-    'default': dj_database_url.config()
-       
-    
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
